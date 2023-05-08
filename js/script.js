8c9f105d93e513f1dbeba4c264d0b7fb396b318b
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-08 19:05:27 +0200

    aggiunti fx script

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
-const {createApp}  = Vue; 
-createApp ({
-    data() {
-        return {
-          titolo: "Todo List",
-          todos: [],
-          newTodo: "",
-        };
-      },
-      methods: {
-         getTodos() {
-            axios.get('api.php').then((response)=> {
-                this.todos = response.data;
-                console.log(response.data);
-              });
-        },
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      titolo: "Todo List",
+      todos: [],
+      newTodo: "",
+    };
+  },
+  methods: {
+    getTodos() {
+      axios.get('api.php').then((response) => {
+        this.todos = response.data;
+        console.log(response.data);
+      });
+    },
+    addTodo() {
+      if (this.newTodo.trim() !== "") {
+        this.todos.push(this.newTodo.trim());
+        this.newTodo = "";
+        this.saveTodos();
         
-      }, 
-      mounted() {
-        this.getTodos();
+      }
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+      this.saveTodos();
+    },
+    toggleCompleted(index) {
+        this.todos[index].completed = !this.todos[index].completed;
+        this.saveTodos();
       },
+    saveTodos() {
+      axios.post('api.php', { todos: this.todos }).then((response) => {
+        console.log(response.data);
+      });
+    },
+  },
+  mounted() {
+    this.getTodos();
+  },
 }).mount("#app");
